5bec2e1fd4704bf06baf6a34acad1e2c2f5086c3
Rameez Kakodker <49229745+medicantBias117@users.noreply.github.com>
2023-12-30 12:42:50 +0400

    Properly removed buttons

--- astro/src/components/main/Button.tsx
+++ astro/src/components/main/Button.tsx
@@ -1,43 +1,43 @@
-import { useState } from "react";
-import { RiFilter3Fill, RiSortDesc, RiFileUserFill } from "react-icons/ri";
+// import { useState } from "react";
+// import { RiFilter3Fill, RiSortDesc, RiFileUserFill } from "react-icons/ri";
 
-interface FilterButton {
-  content: string;
-  type: string;
-  isActive: boolean;
-  className?: string;
-}
+// interface FilterButton {
+//   content: string;
+//   type: string;
+//   isActive: boolean;
+//   className?: string;
+// }
 
-export default function FilterButton({ content, type, isActive }) {
-  const [active, setActive] = useState(isActive);
+// export default function FilterButton({ content, type, isActive }) {
+//   const [active, setActive] = useState(isActive);
 
-  function handleClick() {
-    isActive = !isActive;
-    setActive(!active);
-  }
-  const classNames = active
-    ? "active border filter-button"
-    : "border filter-button";
-  return (
-    <>
-      <button type="button" className={classNames} onClick={handleClick}>
-        <Icon type={type} />
-        <span className="block">{content}</span>
-      </button>
-    </>
-  );
-}
+//   function handleClick() {
+//     isActive = !isActive;
+//     setActive(!active);
+//   }
+//   const classNames = active
+//     ? "active border filter-button"
+//     : "border filter-button";
+//   return (
+//     <>
+//       <button type="button" className={classNames} onClick={handleClick}>
+//         <Icon type={type} />
+//         <span className="block">{content}</span>
+//       </button>
+//     </>
+//   );
+// }
 
-function Icon({ type }) {
-  switch (type) {
-    case "filter":
-      return <RiFilter3Fill className="icon" />;
-      break;
-    case "sort":
-      return <RiSortDesc className="icon" />;
-      break;
-    default:
-      return <RiFileUserFill />;
-      break;
-  }
-}
+// function Icon({ type }) {
+//   switch (type) {
+//     case "filter":
+//       return <RiFilter3Fill className="icon" />;
+//       break;
+//     case "sort":
+//       return <RiSortDesc className="icon" />;
+//       break;
+//     default:
+//       return <RiFileUserFill />;
+//       break;
+//   }
+// }
