9142e1b0ecade0ec6a6ddbe3c8c86d2a09832b30
Rameez Kakodker <49229745+medicantBias117@users.noreply.github.com>
2024-02-18 18:56:56 +0400

    Added Robots.txt

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import robots from "astro-robots"; // Add code manually
 import { sanityIntegration } from "@sanity/astro";
 
 // https://astro.build/config
@@ -10,7 +10,35 @@ export default defineConfig({
   integrations: [
     sitemap(),
     tailwind(),
-
+    robots({
+      host: "https://mendicantbias.com",
+      sitemap: [
+        "https://mendicantbias.com/sitemap.xml",
+        "https://www.mendicantbias.com",
+      ],
+      policy: [
+        {
+          userAgent: [
+            "Applebot",
+            "Googlebot",
+            "bingbot",
+            "Yandex",
+            "Yeti",
+            "Baiduspider",
+            "360Spider",
+            "*",
+          ],
+          allow: ["/"],
+          disallow: ["/admin", "/login"],
+          crawlDelay: 5,
+          cleanParam: ["sid /", "s /forum/showthread"],
+        },
+        {
+          userAgent: "BLEXBot",
+          disallow: ["/assets", "/uploades/1989-08-21/*jpg$"],
+        },
+      ],
+    }),
     sanityIntegration({
       projectId: "1s3iny7g",
       dataset: "production",
